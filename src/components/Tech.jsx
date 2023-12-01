4e27ad583400b4590fd71a66ec8700b2eb857518
Michael Heinhold <michael.heinhold@gmail.com>
2023-11-30 22:13:53 -0700

    browserview

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserView, MobileView } from "react-device-detect";
 
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
@@ -8,9 +9,11 @@ const Tech = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          {/* <BallCanvas icon={technology.icon} /> */}
-        </div>
+        <BrowserView>
+          <div className='w-28 h-28' key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        </BrowserView>
       ))}
     </div>
   );
